97ed0d15bca0c5f87a892374f58fb34e71588163
Olafur Gislason <olafurgi@gmail.com>
2017-06-27 12:13:32 +0000

    Add govend to prepare

--- prepare.bat
+++ prepare.bat
@@ -5,3 +5,9 @@ go get -u github.com/jteeuwen/go-bindata/...
 
 echo Making bindata.go
 %GOPATH%\bin\go-bindata assets\
+
+echo Getting govend
+go get -u github.com/govend/govend
+
+echo Getting vendor packages
+%GOPATH%\bin\govend
